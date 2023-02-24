c04aff3bd774d1e4a87227a7d7f6cda8ce146512
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-24 20:59:16 +0500

    prepare detail view controller

---
+++ APOD(NASA)/ViewControllers/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  APOD(NASA)
+//
+//  Created by Yuriy on 24.02.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var pictureHDImage: UIImageView!
+    @IBOutlet var descriptionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
